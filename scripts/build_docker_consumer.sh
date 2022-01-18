2b079156fc0f8c7524b7e982f8bd38df6ed7e101
hspadim <henrique@spadim.com.br>
2022-01-17 23:45:41 -0300

    Modify main folder

--- scripts/build_docker_consumer.sh
+++ scripts/build_docker_consumer.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # -f: Nome do arquivo Dockerfile
 # -t: Adiciona tag para a imagem
-# "private": o contexto(pasta) em que a imagem
+# "src": o contexto(pasta) em que a imagem
 # sera 'buildada'
-pushd private/consumer
+pushd src/consumer
 source build.sh
 popd
 docker build \
     -f docker/consumer/Dockerfile \
     -t hspadim/red-panda-consumer \
-    private
+    src
--- scripts/build_docker_producer.sh
+++ scripts/build_docker_producer.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # -f: Nome do arquivo Dockerfile
 # -t: Adiciona tag para a imagem
-# "private": o contexto(pasta) em que a imagem
+# "src": o contexto(pasta) em que a imagem
 # sera 'buildada'
-pushd private/producer
+pushd src/producer
 source build.sh
 popd
 docker build \
     -f docker/producer/Dockerfile \
     -t hspadim/red-panda-producer \
-    private
+    src
